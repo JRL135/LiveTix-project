8e014b3cf9ceda788ba2a46cd2628e9884024c0e
JRL <ID+username@users.noreply.github.com>
2022-07-06 17:05:11 +0800

    modify header to differentiate profile, signup/login, signout options for token/no token situations

--- public/components/header.js
+++ public/components/header.js
@@ -5,7 +5,9 @@ class Header extends HTMLElement {
       super();
     }
     connectedCallback() {
-        this.innerHTML = `
+        let token = localStorage.getItem('token');
+        if (token) {
+            this.innerHTML = `
             <div class="header-container navbar">
                 <div class="header-left">
                     <a target="_parent" href="${ROOT_URL}index.html" class="nav-link"><img id="logo-id" src="../img/LiveTixLogo.png"></a>
@@ -15,12 +17,28 @@ class Header extends HTMLElement {
                 </div>
                 <div class="header-right">
                     <a target="_parent" href="${ROOT_URL}profile.html" class="nav-link slide">Profile</a>
+                
+                    <div class="nav-link" id="signout-div">Sign Out</div>
+                </div>
+            </div>`;
+        } else {
+        this.innerHTML = `
+            <div class="header-container navbar">
+                <div class="header-left">
+                    <a target="_parent" href="${ROOT_URL}index.html" class="nav-link"><img id="logo-id" src="../img/LiveTixLogo.png"></a>
+                    <a target="_parent" href="${ROOT_URL}index.html?category=concert" class="nav-link slide">Concert</a>
+                    <a target="_parent" href="${ROOT_URL}index.html?category=festival" class="nav-link slide">Festival</a>
+                    <a target="_parent" id="search-btn" class="nav-link slide">Search</a>
+                </div>
+                <div class="header-right">
+                    
                     <a href="${ROOT_URL}signup.html" class="nav-link slide">Sign Up</a>
                     <a href="${ROOT_URL}login.html" class="nav-link slide">Log In</a>
-                    <div class="nav-link" id="signout-div">Sign Out</div>
+                    
                 </div>
             </div>
         `;
+        }
         let search = document.getElementById('search-btn');
         search.setAttribute('href', `${ROOT_URL}search.html`);
 
