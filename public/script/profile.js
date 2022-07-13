6b45b219850764f469ea70fe233704a2d2b037c8
JRL <ID+username@users.noreply.github.com>
2022-07-13 17:16:36 +0800

    [style] improve profile page event & ticket cards styling

--- public/script/profile.js
+++ public/script/profile.js
@@ -45,12 +45,19 @@ async function getUserRegisteredEvents(username){
     console.log(registeredEvents);
     let registeredEventDiv = document.getElementsByClassName('tab1-div')[0];
     for (let i = 0; i < registeredEvents.length; i++) {
+        let date;
         let start_date = registeredEvents[i].start_date.split('T')[0];
+        let end_date = registeredEvents[i].end_date.split('T')[0];
+        if (start_date === end_date) {
+            date = start_date;
+        } else {
+            date = `${start_date} - ${end_date}`;
+        }
         registeredEventDiv.innerHTML += `
         <div class="registered-event-div">
             <img src="${registeredEvents[i].main_picture}">
-            <div>${registeredEvents[i].title}</div>
-            <div>${start_date}</div>
+            <div id="register-event-div-title">${registeredEvents[i].title}</div>
+            <div id="register-event-div-date">${date}</div>
             <div>${registeredEvents[i].venue} @ ${registeredEvents[i].city}</div>
             <a target="_parent" href="${ROOT_URL}ticket.html?id=${registeredEvents[i].ticket_id}"><button id="ticket-btn">Ticket</button></a>
             
@@ -69,12 +76,19 @@ async function getUserFavEvents(username){
     let favEventsDiv = document.getElementsByClassName('tab2-div')[0];
     console.log("length: " + favEvents.length);
     for (let i = 0; i < favEvents.length; i++) {
+        let date;
         let start_date = favEvents[i][0].start_date.split('T')[0];
+        let end_date = favEvents[i][0].end_date.split('T')[0];
+        if (start_date === end_date) {
+            date = start_date;
+        } else {
+            date = `${start_date} - ${end_date}`;
+        }
         favEventsDiv.innerHTML += `
         <div class="fav-event-div">
             <img src="${favEvents[i][0].main_picture}">
-            <div>${favEvents[i][0].title}</div>
-            <div>${start_date}</div>
+            <div id="fav-event-div-title">${favEvents[i][0].title}</div>
+            <div id="fav-event-div-date">${date}</div>
             <div>${favEvents[i][0].venue} @ ${favEvents[i][0].city}</div>
             <a target="_parent" href="${ROOT_URL}event.html?id=${favEvents[i][0].event_id}"><button id="event-page-btn">Details</button></a>
         </div>
