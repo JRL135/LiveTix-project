dfe319233914dcd26f0a3c027fe446352a9ee0e2
JRL <ID+username@users.noreply.github.com>
2022-07-18 11:33:48 +0800

    [fix] block unfinished purchase from showing up in user profile

--- models/user-model.js
+++ models/user-model.js
@@ -19,7 +19,7 @@ const registerUser = async (email, name, password, role)=>{
 const getUserUnusedTickets = async (username)=>{
   const [userIdDB] = await pool.query(`SELECT user_id FROM users WHERE name = ?`, username);
   const userId = userIdDB[0].user_id;
-  const [registeredEvents] = await pool.query(`SELECT t1.category as category, t1.title as title, DATE_FORMAT(t1.start_date, '%Y-%m-%d') as start_date, DATE_FORMAT(t1.end_date, '%Y-%m-%d') as end_date, t1.city as city, t1.venue as venue, t1.main_picture as main_picture, t2.ticket_id as ticket_id, t2.type_name as ticket_type_name, DATE_FORMAT(t2.purchase_date, '%Y-%m-%d') as purchase_date FROM events t1 INNER JOIN tickets t2 ON t1.event_id = t2.event_id WHERE user_id = ? AND t2.used_status = '0' ORDER BY purchase_date ASC`, userId);
+  const [registeredEvents] = await pool.query(`SELECT t1.category as category, t1.title as title, DATE_FORMAT(t1.start_date, '%Y-%m-%d') as start_date, DATE_FORMAT(t1.end_date, '%Y-%m-%d') as end_date, t1.city as city, t1.venue as venue, t1.main_picture as main_picture, t2.ticket_id as ticket_id, t2.type_name as ticket_type_name, DATE_FORMAT(t2.purchase_date, '%Y-%m-%d') as purchase_date FROM events t1 INNER JOIN tickets t2 ON t1.event_id = t2.event_id WHERE user_id = ? AND t2.used_status = '0' AND t2.purchase_date is not null ORDER BY purchase_date ASC`, userId);
   return registeredEvents;
 };
 
